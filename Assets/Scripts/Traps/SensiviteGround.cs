ff924b80d83895fcaf7926d1a7aacdbfcb95d8ff
Florianus04 <saffanduygun@gmail.com>
2024-01-27 00:24:15 +0300

    sensivite platform

--- Assets/Scripts/Traps/SensiviteGround.cs
+++ Assets/Scripts/Traps/SensiviteGround.cs
@@ -9,47 +9,56 @@ public class SensiviteGround : MonoBehaviour
     public Sprite brokenSprite;
     private void OnEnable()
     {
+        //obje normal sprite ile respawn olsun
         ChangeSprite(normalSprite);
     }
     private void OnDisable()
     {
-        StartCoroutine(RespawnObject());
-    }
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            StartCoroutine(StartBreak());
-        }
+        //3 saniye sonra objeyi yeniden ac
+        Invoke("StartRespawn", 3f);
     }
     void ChangeSprite(Sprite sprite)
     {
+        //sprite degistir
         this.gameObject.GetComponent<SpriteRenderer>().sprite = sprite;
+    }
+    void StartRespawn()
+    {
+        //objeyi yeniden ac
+        this.gameObject.SetActive(true);
     }
     IEnumerator StartBreak()
     {
+        //10 kere sallanti komutu cagir
         for(int i = 0 ; i < 10 ; i++)
         {
-            transform.DOShakePosition(1f, 0.05f);
+            transform.DOShakePosition(1f, 0.04f);
         }
-       
+        
+        //bekle
         yield return new WaitForSeconds(1f);
 
+        //kirik sprite gec
         ChangeSprite(brokenSprite);
 
+        //10 kere daha siddetli sallanti komutu cagir
         for (int i = 0 ; i < 10 ; i++)
         {
-            transform.DOShakePosition(2f, 0.08f);
+            transform.DOShakePosition(2f, 0.06f);
         }
 
+        //bekle
         yield return new WaitForSeconds(2f);
 
+        //objeyi kapat
         this.gameObject.SetActive(false);
     }
-    IEnumerator RespawnObject()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        yield return new WaitForSeconds(3f);
-
-        this.gameObject.SetActive(true);
+        //karakter platforma bastiginda kirilma baslasin
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            StartCoroutine(StartBreak());
+        }
     }
 }
