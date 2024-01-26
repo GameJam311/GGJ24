f202e4c585e674f030ea3e1ec2f76001c4453922
Florianus04 <saffanduygun@gmail.com>
2024-01-26 21:45:25 +0300

    moving platform

--- Assets/Scripts/Traps/MovingPlatform.cs
+++ Assets/Scripts/Traps/MovingPlatform.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class MovingPlatform : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed = 5f;
 
-    // Update is called once per frame
+    public Transform startPos;
+    public Transform endPos;
+
+    private Transform target;
+    private void Start()
+    {
+        target = endPos;
+    }
     void Update()
     {
-        
+        float Movement = speed * Time.deltaTime;
+
+        transform.position = Vector3.MoveTowards(transform.position, target.position, Movement);
+
+        float distanceToTarget = Vector3.Distance(transform.position, target.position);
+
+        if (distanceToTarget < 0.01f)
+        {
+            ChangeDirection();
+        }
+    }
+    void ChangeDirection()
+    {
+        if(target == endPos)
+        {
+            target = startPos;
+        }
+        else
+        {
+            target = endPos;
+        }
     }
 }
